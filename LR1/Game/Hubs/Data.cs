346d76c23c0d07cc4d87099af096ff43dc8f5b4e
Yevhenii Presaizen <midnightzp95@gmail.com>
2020-04-12 21:03:10 +0300

    Create project

---
+++ LR1/Game/Hubs/Data.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game.Hubs
+{
+    public class Data : Hub
+    {
+        public async Task ChangeColor(int id, string color)
+        {
+            await Clients.All.SendAsync("ChangeColorInBrowser", id, color);
+        }
+    }
+}
